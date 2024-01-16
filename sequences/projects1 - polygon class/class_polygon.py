0589929311554e4c086f9a2c001818dd84a9b626
juan felipe hernandez <juanfelipehdezm@example.com>
2024-01-16 17:07:12 -0500

    polygon class

--- sequences/projects1 - polygon class/class_polygon.py
+++ sequences/projects1 - polygon class/class_polygon.py
@@ -19,8 +19,10 @@ We need to create a Polygon class with the following properties:
 
 import math
 
-class polygon:
+class Polygon:
     def __init__(self, n : int, R: float):
+        if n < 3:
+            raise ValueError("Polygon must have at least 3 sides")
         self.n = n
         self.R = R
     
@@ -59,9 +61,26 @@ class polygon:
     def perimeter(self):
         return round(self.n * self.side_length,3)
     
+    #method used to compare the equality of two intances of the same class
+    def __eq__(self, other_polygon):
+        if isinstance(other_polygon, self.__class__):
+            return self.n_of_edges == other_polygon.n_of_edges and \
+                   self.circumradius == other_polygon.circumradius
+        else:
+            return NotImplemented
+                   
+    def __gt__(self, other_polygon):
+        if isinstance(other_polygon, Polygon): #Polygon or self.__class__ represent the class so both are valid
+            return self.n_of_vertices > other_polygon.n_of_vertices
+        else:
+            return NotImplemented
 
     
-polygon = polygon(3, 3.4)
+polygon1 = Polygon(3, 3)
+polygon2 = Polygon(5,5)
 
-print(polygon.__repr__)
+print(polygon2.__gt__(polygon1))
+
+
+#print(polygon.apothem)
 
