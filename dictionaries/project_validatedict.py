da832587178a11c9f51e9600e7962ac987df6b61
juan felipe hernandez <juanfelipehdezm@example.com>
2024-02-12 18:03:14 -0500

    piece of code to compare whether is a dictionary with data has the same kays and data types of a provided template.

--- dictionaries/project_validatedict.py
+++ dictionaries/project_validatedict.py
@@ -33,16 +33,86 @@ template = {
 }
 
 def extract_keys(some_dict: dict) -> list:
+    """
+        Extracts all keys from a dictionary, including nested dictionaries.
+
+        Args:
+            some_dict (dict): The dictionary from which to extract keys.
+
+        Returns:
+            list: A list of all keys extracted from the dictionary.
+
+        Examples:
+            >>> my_dict = {'a': 1, 'b': {'c': 2, 'd': {'e': 3}}}
+            >>> extract_keys(my_dict)
+            ['a', 'b', 'c', 'd', 'e']
+    """
     list_of_keys = []
     for k, v in some_dict.items():
         list_of_keys.append(k)
         if isinstance(v, dict):
-            nested_keys = extract_keys(some_dict[k])
+            nested_keys = extract_keys(v)
             list_of_keys.extend(iter(nested_keys))
     return list_of_keys
 
 
+
+def extract_values(some_dict:dict) -> list:
+    """
+        Extracts all values from a dictionary, including nested dictionaries.
+
+        Args:
+            some_dict (dict): The dictionary from which to extract values.
+
+        Returns:
+            list: A list of all values extracted from the dictionary.
+
+        Examples:
+            >>> my_dict = {'a': 1, 'b': {'c': 2, 'd': {'e': 3}}}
+            >>> extract_values(my_dict)
+            [1, 2, 3]
+    """
+    values = []
+    stack = [some_dict]
+    #print(stack)
+
+    while stack:
+        current_dict = stack.pop()
+        for value in current_dict.values():
+            if isinstance(value, dict):
+                stack.append(value)
+            else:
+                values.append(value)
+    return values
+        
+
+
 def match_keys(data, template):
+    """
+        Checks if the keys in a given data dictionary match the keys in a template dictionary.
+
+        Args:
+            data: The data dictionary to be checked.
+            template: The template dictionary containing the expected keys.
+
+        Returns:
+            Union[str, bool]: If the keys match, returns True. If there are missing or extra keys, returns an error message as a string.
+
+        Examples:
+            >>> data = {'a': 1, 'b': 2}
+            >>> template = {'a': 1, 'b': 2, 'c': 3}
+            >>> match_keys(data, template)
+            'Missing Keys = c'
+            >>> data = {'a': 1, 'b': 2, 'c': 3}
+            >>> template = {'a': 1, 'b': 2}
+            >>> match_keys(data, template)
+            'Extra Keys = c'
+            >>> data = {'a': 1, 'b': 2}
+            >>> template = {'a': 1, 'b': 2}
+            >>> match_keys(data, template)
+            True
+    """
+
     data_keys, template_keys = set(extract_keys(data)), set(extract_keys(template))
 
     missing_error_msg = " "
@@ -66,11 +136,58 @@ def match_keys(data, template):
     else:
         final_error_msg = extra_error_msg
 
-    return final_error_msg or True
+
+    if len(final_error_msg) < 5:
+        return True, "None missing keys"
+    else: return final_error_msg
 
 
-def compare_value_data_types(data, template):
-    
+
+
+def match_values_types(data, template):
+    """
+        Compares the data types of corresponding values in two dictionaries.
+
+        Args:
+            data: The first dictionary for comparison.
+            template: The second dictionary for comparison.
+
+        Returns:
+            Union[str, List[str]]: If the number of values in the dictionaries is different, returns a string indicating a missing key. Otherwise, returns a list of strings indicating data type mismatches for corresponding values.
+
+        Examples:
+            >>> data = {'a': 1, 'b': 'two', 'c': [1, 2, 3]}
+            >>> template = {'a': int, 'b': str, 'c': list}
+            >>> match_values_types(data, template)
+            ['Data type mismatch at value two: expected <class 'int'>, got <class 'str'>', 'Data type mismatch at value [1, 2, 3]: expected <class 'str'>, got <class 'list'>']
+            >>> data = {'a': 1, 'b': 'two', 'c': [1, 2, 3]}
+            >>> template = {'a': int, 'b': str, 'c': dict}
+            >>> match_values_types(data, template)
+            ['Data type mismatch at value [1, 2, 3]: expected <class 'dict'>, got <class 'list'>']
+            >>> data = {'a': 1, 'b': 'two', 'c': [1, 2, 3]}
+            >>> template = {'a': int, 'b': str, 'c': list}
+            >>> match_values_types(data, template)
+            'There is a missing key so no comparison is being made'
+    """
+
+    data_values, template_types = extract_values(data), extract_values(template)
+
+    print("values: ",data_values)
+    print("expected types: ", template_types)
+
+    if len(data_values) != len(template_types):
+        return "There is a missing key so no comparison is being made"
+    else:
+        return [
+            f"Data type mismatch at value {data_values[i]}: expected {expected_type}, got {type(value)}"
+            for i, (value, expected_type) in enumerate(
+                zip(data_values, template_types)
+            )
+            if not isinstance(value, expected_type)
+        ]
+
+
+
 
 eric = {
     'user_id': 101,
@@ -90,5 +207,28 @@ eric = {
     }
 }
 
+michael = {
+    'user_id': 102,
+    'name': {
+        'first': 'Michael',
+        'last': 'Palin'
+    },
+    'bio': {
+        'dob': {
+            'year': 1943,
+            'month': 'May',
+            'day': 5
+        },
+        'birthplace': {
+            'country': 'United Kingdom',
+            'city': 'Sheffield'
+        }
+    }
+}
+
 print(match_keys(eric, template))
-print(compare_value_data_types(eric,template))
+
+print(match_values_types(eric,template))
+
+
+
